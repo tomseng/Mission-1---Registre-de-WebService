9d87a305773674363f03783c42130837b4a94fb5
tomseng <engelsthomas92@gmail.com>
2016-05-20 15:51:55 +0200

    Menu
    ajout du menu pour naviguer dans les fonctions

--- src/test/Test1.java
+++ src/test/Test1.java
@@ -12,39 +12,53 @@ import modele.WebService;
 public class Test1 {
 
 	public static void main(String[] args) throws Exception {
+		
+		System.out.println("Bienvenue dans l'espace d'ajout de WS "+  "\n" +  "\n" 
+		+" 0 - Ajouter un Web Service" +  "\n" 
+	    + " 1 - Afficher tous les Web Services répertoriés dans le registre " + "\n"
+		+ " 2 - Afficher les détails d'un Web Service" +  "\n"
+	    + "Choisissez un chiffre pour utiliser une fonctionnalité");
+		Scanner sc = new Scanner(System.in);
+	int choix = sc.nextInt();
+		if (choix == 0){
+			ajoutWS();
+		}
+		else {
+			System.out.println("ce choix n'est pas disponible");	
+			
+		}
+		
+	}
+	
+	public static String ajoutWS(){
 		Configuration config = new Configuration();
 		SessionFactory sessionFactory = config.configure().buildSessionFactory();
-		
-		
-		
+			
 		// Ouverture session
-		Session session = sessionFactory.openSession() ;
-		session.close();
-		session = sessionFactory.openSession();
-		Transaction tx = session.beginTransaction();
-		System.out.println("Ajouter un Web Service: Saissez le nom");
-		WebService wbs1 = new WebService();
-		Scanner sc = new Scanner(System.in);
-		String nomWS = sc.nextLine();
-		wbs1.setNomWS(nomWS);
-		System.out.println("Ajouter un Web Service: Saissez le type");
-		String typeWS = sc.nextLine();
-		wbs1.setTypeWS(typeWS);
-		System.out.println("Ajouter un Web Service: Saissez le commentaire");
-		String commentaireWS = sc.nextLine();
-		wbs1.setCommentaireWS(commentaireWS);
-		sc.close();
+				Session session = sessionFactory.openSession() ;
+				session.close();
+				session = sessionFactory.openSession();
+				Transaction tx = session.beginTransaction();
+				System.out.println("Ajouter un Web Service: Saissez le nom");
+				WebService wbs1 = new WebService();
+				Scanner sc = new Scanner(System.in);
+				String nomWS = sc.nextLine();
+				wbs1.setNomWS(nomWS);
+				System.out.println("Ajouter un Web Service: Saissez le type");
+				String typeWS = sc.nextLine();
+				wbs1.setTypeWS(typeWS);
+				System.out.println("Ajouter un Web Service: Saissez le commentaire");
+				String commentaireWS = sc.nextLine();
+				wbs1.setCommentaireWS(commentaireWS);
+				sc.close();
 
-		session.save(wbs1);
-		System.out.println("Nouveau Web Service ajouté!");
-		
-		tx.commit();
-		System.out.println("WS sauvegardé");
-		session.close();
-		
-		
-		
-		
+				session.save(wbs1);
+				System.out.println("Nouveau Web Service ajouté!");
+				
+				tx.commit();
+				System.out.println("WS sauvegardé");
+				session.close();
+		return null;
 	}
 
 }
